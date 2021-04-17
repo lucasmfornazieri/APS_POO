3ac93f82b3e74138e1f618777b04b57d1ad8675e
CarolineMano <carol.marinho.mano@gmail.com>
2021-04-17 13:35:54 -0300

    Create Homem.java
    Criação da Classe Homem

---
+++ Homem.java
@@ -0,0 +1,27 @@
+public class Homem {
+    //Atributos
+    private int posicaoX;
+    private int posicaoY;
+
+    //Construtor
+    public Homem() {
+        this.posicaoX = 4;
+        this.posicaoY = 0;
+    }
+
+    public int getPosicaoX() {
+        return posicaoX;
+    }
+
+    public int getPosicaoY() {
+        return posicaoY;
+    }
+
+    public void setPosicaoX(int posicaoX) {
+        this.posicaoX = posicaoX;
+    }
+
+    public void setPosicaoY(int posicaoY) {
+        this.posicaoY = posicaoY;
+    }
+}
