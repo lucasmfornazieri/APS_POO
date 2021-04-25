51063c10f97ad87905c09e313a49cedd54acc6cb
lucasmfornazieri <73902861+lucasmfornazieri@users.noreply.github.com>
2021-04-25 16:29:41 -0300

    Update Main.java
    apenas modificando para 20 pra poder tirar os prints

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Homem homenzinho = new Homem();
-        Mapa mapinha = new Mapa(homenzinho, 10);
+        Mapa mapinha = new Mapa(homenzinho, 20);
         mapinha.mostrarMapa();
 
         mapinha.acharCaminhoAutomaticamente(homenzinho, homenzinho.getPosicaoX(), homenzinho.getPosicaoY());
